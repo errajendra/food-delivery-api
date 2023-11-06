0e6a0bbed5898ab442cca7a3ddb0b22b184fc4c2
bhawna gupta <bhawnagupta@bhawnas-MacBook-Air.local>
2023-11-06 19:06:12 +0530

    Meal request daily

--- user/ajax/urls.py
+++ user/ajax/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     #update user active url
     path('update-user-active/', update_user_active, name='update-user-active-ajax'),
     path('update-plan-purchase-active/', update_plan_purchase_active, name='update-plan-purchase-active-ajax'),
+    
+    path('update-meal-active-ajax/', update_meal_active_ajax, name='update-meal-active-ajax'),
+    
+    
 ]
+
--- user/ajax/views.py
+++ user/ajax/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http.response import JsonResponse
 from ..models import CustomUser as User
-from meal.models import PlanPurchase
+from meal.models import PlanPurchase, MealRequestDaily
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
@@ -29,4 +29,17 @@ def update_plan_purchase_active(request):
         plan.save()
         return JsonResponse({'success': True})
     except User.DoesNotExist:
-        return JsonResponse({'success': False, 'error': 'User not found'})
+        return JsonResponse({'success': False, 'error': 'Plan not found'})
+    
+    
+def update_meal_active_ajax(request):
+    meal_id = request.POST.get('meal_id')
+    new_status = int(request.POST.get('new_status'))
+    try:
+        meal = MealRequestDaily.objects.get(pk=meal_id)
+        meal.delivered = bool(new_status)
+        meal.save()
+        return JsonResponse({'success': True})
+    except User.DoesNotExist:
+        return JsonResponse({'success': False, 'error': 'Meal not found'})
+        
