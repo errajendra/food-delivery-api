858d1b681bd68db5c5b007c10d25096aae88b2ae
rajendraswooshelf <rajendras@wooshelf.com>
2023-11-09 18:03:35 +0530

    sfisuhs

--- FoodDelivery/settings.py
+++ FoodDelivery/settings.py
@@ -145,9 +145,7 @@ USE_TZ = True
 
 LOGIN_URL = "/user/login/"
 
-# CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_URLS_REGEX = r"^/api/.*$"
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
