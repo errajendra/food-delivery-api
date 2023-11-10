38b677b729e220d755ef087ba6bb1fc4b02a4da9
rajendraswooshelf <rajendras@wooshelf.com>
2023-11-10 15:07:32 +0530

    statics

--- FoodDelivery/settings.py
+++ FoodDelivery/settings.py
@@ -167,7 +167,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email Configration Credentials
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = os.environ.get("EMAIL_PORT")
 EMAIL_USE_TLS = True
