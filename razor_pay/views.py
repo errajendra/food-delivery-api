f00c033a7c125295216e24bece802d074e9ad547
rajendraswooshelf <rajendras@wooshelf.com>
2024-01-01 11:14:54 +0530

    razorpay signature error

--- razor_pay/views.py
+++ razor_pay/views.py
@@ -13,13 +13,24 @@ def verify_payment(request):
     razorpay_order_id = request.data['razorpay_order_id']
     razorpay_payment_id = request.data['razorpay_payment_id']
     razorpay_signature = request.data['razorpay_signature']
-    result = client.utility.verify_payment_signature(
-        {
-            'razorpay_order_id': razorpay_order_id,
-            'razorpay_payment_id': razorpay_payment_id,
-            'razorpay_signature': razorpay_signature
-        }
-    )
+    try:
+        result = client.utility.verify_payment_signature(
+            {
+                'razorpay_order_id': razorpay_order_id,
+                'razorpay_payment_id': razorpay_payment_id,
+                'razorpay_signature': razorpay_signature
+            }
+        )
+    except Exception as ex:
+        print(ex)
+        return Response(
+            data={
+                'status': 400,
+                'message': 'Invalid Payment.',
+                'data': f"Signature not valid. - {ex}"
+            },
+            status=400
+        )
     if result:
         tnx = get_object_or_404(Transaction, tracking_id=razorpay_order_id)
         tnx.status = "Success"
