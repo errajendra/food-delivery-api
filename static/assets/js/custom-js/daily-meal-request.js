bc121946880e2fa798df94760a2936b518644c63
rajendraswooshelf <rajendras@wooshelf.com>
2024-04-05 16:39:50 +0530

    First Address length issue Date range Selection on Daily Meal Request None in Addess (mobile number) backend Menu not showing (working fine from backend)

--- static/assets/js/custom-js/daily-meal-request.js
+++ static/assets/js/custom-js/daily-meal-request.js
@@ -120,3 +120,30 @@ $(document).ready(function() {
     });
 
 } );
+
+
+// Search data by date range (for orders and view delivery delivered) 
+$(function() {
+  var table = $("#example1").DataTable();    
+  // Date range vars
+  minDateFilter = "";
+  maxDateFilter = "";  
+  $("#reservation").daterangepicker();
+  $("#reservation").on("apply.daterangepicker", function(ev, picker) {
+    minDateFilter = Date.parse(picker.startDate);
+    maxDateFilter = Date.parse(picker.endDate);
+    $.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
+    var date = Date.parse(data[6]);  
+    if (
+    (isNaN(minDateFilter) && isNaN(maxDateFilter)) ||
+    (isNaN(minDateFilter) && date <= maxDateFilter) ||
+    (minDateFilter <= date && isNaN(maxDateFilter)) ||
+    (minDateFilter <= date && date <= maxDateFilter)
+    ) {
+    return true;
+    }
+    return false;
+  });
+  table.draw();
+  });
+});
